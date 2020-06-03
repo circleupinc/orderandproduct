0e82e3310bbe148723dd043667f80927694c86b8
pranto <sharoarjahan.iut@gmail.com>
2020-06-03 13:45:23 +0600

    new

---
+++ productandorder/src/main/java/com/circleupinc/productandorder/Order/Fulfillment.java
@@ -0,0 +1,16 @@
+package com.circleupinc.productandorder.Order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.circleupinc.productandorder.R;
+
+public class Fulfillment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fulfillment);
+    }
+}
